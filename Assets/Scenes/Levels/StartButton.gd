9982eb0860571a77dc97d853589d888a952b0cbb
TheRubeThing <therubething@gmail.com>
2020-12-16 19:10:45 +0100

    Added level select

--- Assets/Scenes/Levels/StartButton.gd
+++ Assets/Scenes/Levels/StartButton.gd
@@ -5,4 +5,4 @@ export(PackedScene) var next_level
 func _pressed():
 	$AudioStreamPlayer.play()
 	yield(get_tree().create_timer(0.5), "timeout")
-	SceneManager.goto_level(1)
+	SceneManager.goto_level(GameData.current_level)
